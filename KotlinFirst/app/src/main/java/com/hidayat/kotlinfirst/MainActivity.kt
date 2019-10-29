f86d18145f0a5d804bd2bcc43f2cbce287c34958
yat98 <hidayatchandra08@gmail.com>
2019-10-29 18:33:12 +0800

    Menambahkan tombol ubah judul dan fungsinya

--- KotlinFirst/app/src/main/java/com/hidayat/kotlinfirst/MainActivity.kt
+++ KotlinFirst/app/src/main/java/com/hidayat/kotlinfirst/MainActivity.kt
@@ -2,11 +2,24 @@ package com.hidayat.kotlinfirst
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var tvJudul = findViewById(R.id.tv_judul) as TextView
+        var button = findViewById(R.id.btn_ubah) as Button
+        button.setOnClickListener(View.OnClickListener {
+            val judul = tvJudul.text
+            when(judul){
+                "Hello World!" -> tvJudul.setText("Hello Hidayat")
+                "Hello Hidayat" -> tvJudul.setText("Hello World!")
+            }
+
+        })
     }
 }
