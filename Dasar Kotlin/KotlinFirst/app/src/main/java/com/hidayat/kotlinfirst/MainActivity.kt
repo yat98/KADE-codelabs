469c381eae280c95a232c56a937eb14e35570ebb
yat98 <hidayatchandra08@gmail.com>
2019-10-30 20:08:56 +0800

    Memindahkan projek kotlinfirst kedalam folder dasar kotlin

---
+++ Dasar Kotlin/KotlinFirst/app/src/main/java/com/hidayat/kotlinfirst/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.hidayat.kotlinfirst
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val buttonNumber = findViewById(R.id.btn_number) as Button
+        val tvJudul = findViewById(R.id.tv_judul) as TextView
+        val button = findViewById(R.id.btn_ubah) as Button
+
+        button.setOnClickListener({
+            val judul = tvJudul.text
+            when (judul) {
+                "Hello World!" -> tvJudul.setText("Hello Hidayat")
+                "Hello Hidayat" -> tvJudul.setText("Hello World!")
+            }
+        })
+
+        buttonNumber.setOnClickListener(View.OnClickListener {
+            val numberIntent = Intent(this, NumberActivity::class.java)
+            startActivity(numberIntent)
+        })
+    }
+}
